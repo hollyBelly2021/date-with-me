c2972aac24c4727a8208fbcb8efc33b394a1e749
Maea Matugas <matugasmaea@gmail.com>
2025-06-22 13:21:21 -0500

    simple authentication added

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("JavaScript is ready to run");
+function validateInput() {
+  const input = document.getElementById("input");
+  const inputValue = document.getElementById("input").value;
+  const questionContainer = document.querySelector(".question-container");
+  const main = document.querySelector("main");
+
+  let ans = inputValue.toLowerCase();
+
+  // Example validation: check if it's empty
+  if (ans.trim() === "") {
+    alert("Input cannot be empty.");
+    input.value = "";
+  } else if (ans === "elephant") {
+    // remove display none class
+    main.classList.remove("none-display");
+    // add display none to question-container class
+    questionContainer.classList.add("none-display");
+  } else {
+    alert("Not you.");
+    input.value = "";
+  }
+}
